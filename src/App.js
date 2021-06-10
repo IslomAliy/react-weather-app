bca56b4ab0e83691439614342c38ed5e9fdbad54
islomaliy <kislombek71@gmail.com>
2021-06-10 08:50:06 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import 'weather-icons/css/weather-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Weather from './app_component/weather.component';
+
+// api.openweathermap.org/data/2.5/weather?q={city name}
+const API_KEY = "26901649b35efc546dff0f82dde13fe6";
+
+class App extends React.Component {
+  state = {}
+  render(){
+    return();
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather/>
     </div>
   );
 }
